30ec190cb7b941ad0072743eab14cef2e5e92e1e
Blake Hawkins <blake.hawkins.11@gmail.com>
2019-04-30 20:09:29 +0100

    second parser generator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use] extern crate lalrpop_util;
 
 lalrpop_mod!(pub calculator1);  // Synthesised by LALRPOP.
+lalrpop_mod!(pub calculator2);  // Synthesised by LALRPOP.
 
 #[derive(Debug, Clone)]
 enum Op {
@@ -26,4 +27,12 @@ fn calculator1() {
   assert!(calculator1::TermParser::new().parse("(22)").is_ok());
   assert!(calculator1::TermParser::new().parse("(((69)))").is_ok());
   assert!(calculator1::TermParser::new().parse("((1)").is_err());
+}
+
+#[test]
+fn calculator2() {
+  assert!(calculator2::TermParser::new().parse("22").is_ok());
+  assert!(calculator2::TermParser::new().parse("(22)").is_ok());
+  assert!(calculator2::TermParser::new().parse("(((69)))").is_ok());
+  assert!(calculator2::TermParser::new().parse("((1)").is_err());
 }
